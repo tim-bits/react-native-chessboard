a0d4112190af5d95d3a63dd7d9f3130eaa76a0fc
cstc_acer\Snowden <tim.beats@proton.me>
2025-07-31 15:55:19 -0700

    added    Xwrapper

--- src/components/pieces.tsx
+++ src/components/pieces.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {View} from 'react-native';
 import { useChessboardProps } from '../context/props-context/hooks';
 
 import { useBoard } from '../context/board-context/hooks';
@@ -7,24 +8,50 @@ import { usePieceRefs } from '../context/board-refs-context/hooks';
 import Piece from './piece';
 import { useReversePiecePosition } from '../notation';
 
+import Draggable from 'react-draggable';
+
+import Xarrow, { useXarrow, Xwrapper } from 'react-xarrows';
+
+
 const Pieces = React.memo(() => {
   const board = useBoard();
   const refs = usePieceRefs();
   const { pieceSize } = useChessboardProps();
   const { toPosition } = useReversePiecePosition();
 
+
+// const boxStyle = {/*border: 'grey solid 2px',*/ borderRadius: '10px', padding: '5px'};
+
+const DraggableBox = ({id}:{id:string}) => {
+    // const id = idx;
+    const updateXarrow = useXarrow();
+    return (
+        <Draggable id={id} onDrag={updateXarrow} onStop={updateXarrow}>
+            {/* <div id={id} style={boxStyle}> */}
+                {/* {id} */}
+            {/* </div> */}
+        </Draggable>
+    );
+};
+
   return (
-    <>
+    <Xwrapper>
       {board.map((row, y) =>
         row.map((piece, x) => {
           
           console.log("piece, x, y", piece, x, y)
-          
-          if (piece !== null) {
+
             const square = toPosition({
               x: x * pieceSize,
               y: y * pieceSize,
             });
+
+          
+          if (piece !== null) {
+            // const square = toPosition({
+            //   x: x * pieceSize,
+            //   y: y * pieceSize,
+            // });
 
             return (
               <Piece
@@ -37,10 +64,16 @@ const Pieces = React.memo(() => {
               />
             );
           }
-          return null;
+          // return null;
+          // return <View style={{display:'none'}}/>;
+          return <DraggableBox id={square}/>
         })
       )}
-    </>
+      {/* {arrows.map((arrow, index) => (
+                    <Xarrow key={index} start={arrow.start} end={arrow.end}/>
+      ))} */}
+      <Xarrow start={"f3"} end={"a8"}/>
+    </Xwrapper>
   );
 });
 
