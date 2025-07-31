53ecebe14fe970fc1d4600a0f064a79a5d173e56
cstc_acer\Snowden <tim.beats@proton.me>
2025-07-31 15:09:26 -0700

    revert to original

--- src/components/pieces.tsx
+++ src/components/pieces.tsx
@@ -1,5 +1,4 @@
-import React, {useRef} from 'react';
-// import  {View} from 'react-native';
+import React from 'react';
 import { useChessboardProps } from '../context/props-context/hooks';
 
 import { useBoard } from '../context/board-context/hooks';
@@ -8,112 +7,35 @@ import { usePieceRefs } from '../context/board-refs-context/hooks';
 import Piece from './piece';
 import { useReversePiecePosition } from '../notation';
 
-import Xarrow, {Xwrapper} from "react-xarrows";
-import { PieceType } from 'src/types';
-
 const Pieces = React.memo(() => {
   const board = useBoard();
   const refs = usePieceRefs();
   const { pieceSize } = useChessboardProps();
   const { toPosition } = useReversePiecePosition();
 
-  // const pieceRef = useRef<string>(null);
-  const pieceRef:React.MutableRefObject<string | null> = useRef<string | null>(null);
-  const element:React.MutableRefObject<PieceType | null>= useRef<PieceType | null>(null);
-
   return (
     <>
       {board.map((row, y) =>
         row.map((piece, x) => {
+          
+          console.log("piece, x, y", piece, x, y)
+          
           if (piece !== null) {
-
-
             const square = toPosition({
               x: x * pieceSize,
               y: y * pieceSize,
             });
 
-            console.log("square", square)
-            // pieceRef.current =  refs?.current?.[square] 
-
-
-
-            const id=`${piece.color}${piece.type}` as const
-
-            //  (async ()=>{pieceRef.current = id})()
-
-            // if(square ==='h1') {
-              // pieceRef.current = 'a8';
-            // }
-
-            if (element.current === null) {
-            element.current = <Piece
-                // ref={refs?.current?.[square]}
-                ref = {pieceRef}
+            return (
+              <Piece
+                ref={refs?.current?.[square]}
                 key={`${x}-${y}`}
-                id = {id}
-                // id={`${piece.color}${piece.type}` as const}
+                id={`${piece.color}${piece.type}` as const}
                 startPosition={{ x, y }}
                 square={square}
                 size={pieceSize}
               />
-              pieceRef.current = id;
-            }
-            // if(pieceRef.current) {
-            //   <Xarrow
-            //     start={pieceRef} //can be react ref
-            //     end={id} //or an id
-            //   />
-            // } else {
-            //   pieceRef.current = id;
-            // }
-
-            return (
-              // <Piece
-              //   ref={refs?.current?.[square]}
-              //   key={`${x}-${y}`}
-              //   id = {id}
-              //   // id={`${piece.color}${piece.type}` as const}
-              //   startPosition={{ x, y }}
-              //   square={square}
-              //   size={pieceSize}
-              // />
-              <>
-              {/* {pieceRef.current &&  */}
-              <Xwrapper key={id}>
-                <Piece
-                ref={refs?.current?.[square]}
-                // ref = {pieceRef}
-                key={`${x}-${y}` as const}
-                id = {id}
-                // id={`${piece.color}${piece.type}` as const}
-                startPosition={{ x, y }}
-                square={square}
-                size={pieceSize}/>
-                {element.current}
-                <Xarrow start={pieceRef.current} end={id}/>
-              </Xwrapper>
-              {/* } */}
-                          
-                          
-                {/* {!pieceRef.current && 
-                <Piece
-                ref={refs?.current?.[square]}
-                // ref = {pieceRef}
-                key={`${x}-${y}`}
-                id = {id}
-                // id={`${piece.color}${piece.type}` as const}
-                startPosition={{ x, y }}
-                square={square}
-                size={pieceSize}/>} */}
-              </> 
-
-
             );
-
-
-
-            
           }
           return null;
         })
