4d68e4d63dffb0a72d3e52ec83ad40404035a379
cstc_acer\Snowden <tim.beats@proton.me>
2025-08-22 19:48:22 -0700

    Made arrows show properly for black

--- src/hooks/use-arrows.tsx
+++ src/hooks/use-arrows.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { useChessboardProps } from '../context/props-context/hooks';
 import { ArrowsContext } from '../context/board-refs-context';
 import type { ArrowPair} from '../context/board-refs-context';
-import { useChessEngine } from '../context/chess-engine-context/hooks';
+// import { useChessEngine } from '../context/chess-engine-context/hooks';
 
 type Orientation = 'white' | 'black';
 
@@ -13,53 +13,6 @@ export type VisualWithArrows = {
   arrows: ArrowPair[];
 };
 
-export const useBoardVisualWithArrows = (): VisualWithArrows => {
-  const props = useChessboardProps();
-  const arrows = useContext(ArrowsContext) || [];
-  const chess = useChessEngine();
-  const turn = chess?.turn?.(); // 'w' | 'b'
-  const orientation: Orientation = turn === 'b' ? 'black' : 'white';
-
-  const { boardSize, pieceSize } = props;
-  return { boardSize, squareSize: pieceSize, orientation, arrows };
-};
-
-
-
-// import { useContext } from 'react';
-// import { ChessboardPropsContext } from '../context/props-context';
-// import { ArrowsContext, ArrowPair } from '../context/board-refs-context';
-// import { useChessEngine } from '../context/chess-engine-context/hooks';
-
-// type Orientation = 'white' | 'black';
-
-// export const useBoardVisualWithArrows = () => {
-//   const props = useContext(ChessboardPropsContext);
-//   if (!props) throw new Error('useBoardVisualWithArrows must be used inside providers');
-
-//   const arrows = useContext(ArrowsContext) || [];
-//   const chess = useChessEngine();
-//   const turn = chess?.turn?.();
-//   const orientation: Orientation = turn === 'b' ? 'black' : 'white';
-
-//   const { boardSize, pieceSize } = props;
-//   return { boardSize, squareSize: pieceSize, orientation, arrows };
-// };
-
-
-// import { useContext } from 'react';
-// import { useChessboardProps } from '../props-context'; // <-- if this exists
-// import { ArrowsContext, ArrowPair } from '../board-ref-context';
-// import { useChessEngine } from '../chess-engine-context/hooks';
-
-// type Orientation = 'white' | 'black';
-// export type VisualWithArrows = {
-//   boardSize: number;
-//   squareSize: number; // same as pieceSize
-//   orientation: Orientation;
-//   arrows: ArrowPair[];
-// };
-
 // export const useBoardVisualWithArrows = (): VisualWithArrows => {
 //   const props = useChessboardProps();
 //   const arrows = useContext(ArrowsContext) || [];
@@ -67,7 +20,17 @@ export const useBoardVisualWithArrows = (): VisualWithArrows => {
 //   const turn = chess?.turn?.(); // 'w' | 'b'
 //   const orientation: Orientation = turn === 'b' ? 'black' : 'white';
 
-//   // pieceSize in props is the square size
 //   const { boardSize, pieceSize } = props;
 //   return { boardSize, squareSize: pieceSize, orientation, arrows };
 // };
+
+export const useBoardVisualWithArrows = (): VisualWithArrows => {
+  const props = useChessboardProps();
+  const arrows = useContext(ArrowsContext) || [];
+
+  const orientation: Orientation =
+    (props as any).orientation === 'black' ? 'black' : 'white';
+
+  const { boardSize, pieceSize } = props as any;
+  return { boardSize, squareSize: pieceSize, orientation, arrows };
+};
