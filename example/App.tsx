25e1e64dd66355ddf67e416cc928915aa363a5a4
cstc_acer\Snowden <tim.beats@proton.me>
2025-08-30 16:45:27 -0700

    fixed 'yarn (install)' errors

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Chessboard, { ChessboardRef } from 'react-native-chessboard';
+import Chessboard from 'react-native-chessboard';
+import type { ChessboardRef } from 'react-native-chessboard';
+
 
 export default function App() {
   const chessboardRef = useRef<ChessboardRef>(null);
